a132acb499204f55dc41ca411466bef00773b75a
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2023-06-05 18:07:38 +0200

    Feat: Admin Member Wizard (#10)
    * Feat: initial setup for admins to edit all members
    
    * Fix: Wrong wizard for deleting member
    
    * Feat: Filter by committee

---
+++ src/components/admin/medlemmar/DeleteMemberWizard.tsx
@@ -0,0 +1,44 @@
+import type { FC } from "react";
+import { toast } from "react-hot-toast";
+import Modal from "~/components/layout/Modal";
+import { api } from "~/utils/api";
+
+interface IDeleteOraganWizard {
+  close: () => void;
+  id: string;
+  isOpen: boolean;
+}
+
+export const DeleteMemberWizard: FC<IDeleteOraganWizard> = ({
+  close,
+  id,
+  isOpen,
+}) => {
+  const ctx = api.useContext();
+
+  const { mutate: deleteMember } = api.member.deleteMember.useMutation({
+    onMutate: () => toast.loading("Raderar medlem"),
+    onSettled: (_c, _d, _e, toastId) => {
+      void ctx.member.getAllMembersAsAdmin.invalidate();
+      toast.remove(toastId);
+      close();
+    },
+    onError: () => toast.error("Okänt fel. Försök igen senare!"),
+    onSuccess: () => toast.success("Medlem har raderats!"),
+  });
+
+  return (
+    <Modal
+      buttonText="Radera"
+      isOpen={isOpen}
+      onClose={() => close()}
+      onSubmit={() => deleteMember({ id })}
+      title="Radera medlem"
+    >
+      <div className="mx-auto w-fit max-w-prose">
+        <p>Är du säker på att du vill radera medlemmen?</p>
+        <p>Denna ändring går inte att ångra!</p>
+      </div>
+    </Modal>
+  );
+};
