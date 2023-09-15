5ec7285e1af6f0f4b5a9b5b4377b9d18fc8a92fd
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2023-09-15 13:25:34 +0200

    Chore: Reuse DocumentsAccordionItem component

--- src/pages/documents.tsx
+++ src/pages/documents.tsx
@@ -1,16 +1,9 @@
 import type { GetStaticProps, NextPage } from "next";
-import Link from "next/link";
-import React from "react";
+import DocumentsAccordionItem from "~/components/documents/DocumentsAccordionItem";
 import HeadLayout from "~/components/layout/HeadLayout";
 import SectionTitle from "~/components/layout/SectionTitle";
 import SectionWrapper from "~/components/layout/SectionWrapper";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "~/components/ui/accordion";
-import { buttonVariants } from "~/components/ui/button";
+import { Accordion } from "~/components/ui/accordion";
 import ssg from "~/server/api/helper/ssg";
 import { api } from "~/utils/api";
 
@@ -27,66 +20,13 @@ const DocumentsPage: NextPage = () => {
           </SectionTitle>
           <div className="flex flex-col gap-8">
             <Accordion type="single" collapsible>
-              {documentGroups?.map(
-                ({ Document: documents, extraText, name }, idx) => (
-                  <AccordionItem key={name} value={name}>
-                    <AccordionTrigger>{name}</AccordionTrigger>
-                    <AccordionContent>
-                      <div className="flex flex-col gap-2">
-                        {documents.map(({ isPDF, title, url }) => (
-                          <React.Fragment key={url}>
-                            <div className="flex flex-row items-center justify-between px-2">
-                              <p>{title}</p>
-                              <div className="flex flex-row items-center justify-center gap-2">
-                                {isPDF && (
-                                  <>
-                                    <a
-                                      className={buttonVariants({
-                                        variant: "link",
-                                        size: "sm",
-                                        className: "text-xs font-light",
-                                      })}
-                                      href={url}
-                                      download
-                                    >
-                                      Ladda ner
-                                    </a>
-                                    <p> | </p>
-                                  </>
-                                )}
-                                <Link
-                                  className={buttonVariants({
-                                    variant: "link",
-                                    size: "sm",
-                                    className: "text-xs font-light",
-                                  })}
-                                  href={
-                                    isPDF
-                                      ? `https://docs.google.com/viewer?url=${url}`
-                                      : url
-                                  }
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                                >
-                                  Öppna
-                                </Link>
-                              </div>
-                            </div>
-                            {!(idx === documents.length && !extraText) && (
-                              <div className="border-b border-black" />
-                            )}
-                          </React.Fragment>
-                        ))}
-                        {extraText && (
-                          <div className="mt-2 flex flex-row items-center justify-between px-2 text-xs">
-                            <p>{extraText}</p>
-                          </div>
-                        )}
-                      </div>
-                    </AccordionContent>
-                  </AccordionItem>
-                )
-              )}
+              {documentGroups?.map(({ Document: documents, ...rest }) => (
+                <DocumentsAccordionItem
+                  key={rest.name}
+                  documents={documents}
+                  {...rest}
+                />
+              ))}
             </Accordion>
           </div>
         </SectionWrapper>
