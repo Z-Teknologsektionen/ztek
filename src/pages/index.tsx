d0314e613190f329d7a674d99716f4b8b6fb0796
Kiwimagic03 <oscarmf03@gmail.com>
2023-10-09 20:42:36 +0200

    Added sponsors and frontpage carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,102 @@
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react'; // import from 'keen-slider/react.es' for to get an ES module
 import { type NextPage } from "next";
+import React, { useEffect, useState } from "react";
 
+const Carousel = () => {
+  const [currentSlide, setCurrentSlide] = React.useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider(
+    
+    {
+      loop: true,
+      created() {
+        setLoaded(true)
+      },
+      initial: 0,
+    },
+    [
+      // add plugins here
+    ]
+  )
+
+  useEffect(() => {
+  const interval = setInterval(() => {
+    instanceRef.current?.next()
+  }, 6000);
+  return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div ref={sliderRef} className="keen-slider max-w-[80vw] mx-auto w-full aspect-video rounded-xl flex-1">
+        <div className="keen-slider__slide">
+          <img className='w-full h-full object-cover' src="https://zfoto.ztek.se/img/thumb/20230902-DH6_7840.jpg"/>
+        </div>
+        <div className="keen-slider__slide">
+          <img className='w-full h-full object-cover' src="https://zfoto.ztek.se/img/thumb/20230908-DH8_8364.jpg"/>
+        </div>
+        <div className="keen-slider__slide">
+          <img className='w-full h-full object-cover' src="https://zfoto.ztek.se/img/thumb/20230904-DH8_8302.jpg"/>
+        </div>
+      
+        {loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
+
+            <Arrow
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            />
+          </>
+        )}
+      </div>
+      
+    </>
+  )
+}
+
+function Arrow(props: {
+  disabled: boolean
+  left?: boolean
+  onClick: (e: any) => void
+}) {
+  const disabeld = props.disabled ? " arrow--disabled" : ""
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`w-[30px] h-[30px] absolute top-1/2 fill-white ${
+        props.left ? "left-1" : "right-1"
+      } ${disabeld}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  )
+}
 
 const HomePage: NextPage = () => {
-  var luckyRotate = 5;
   return (
     <>
     <div>
-      <div className="bg-lime-500 relative w-full min-h-[40vh] flex justify-between md:flex-row flex-col mx-auto max-w-[84rem] px-4 sm:px-6 lg:px-8">
+      <div className="relative w-full min-h-[40vh] flex justify-between md:flex-row flex-col mx-auto max-w-[84rem]">
         <div className="bg-white <-TEMPORÄR_TA_BORT! md:w-1/2 flex flex-col gap-4">
           <div className="xl:text-4xl md:text-2xl text-base font-bold">
             Välkommen till Automation och Mekatronik på Chalmers tekniska högskola!
@@ -15,12 +105,7 @@ const HomePage: NextPage = () => {
             Z-teknologsektionen, eller Z som programmet kallas, är civilingengörsprogrammet på Chalmers som beskrivs som länken mellan maskin-, elektro och datateknik.
           </div>
         </div>
-        <div className="flex-1">
-          <div className="bg-orange-500 max-w-[80vw] mx-auto w-full aspect-video">
-
-          </div>
-        </div>
-        
+        <Carousel/>
       </div>
       
       <div className="relative w-full h-screen">
@@ -29,9 +114,16 @@ const HomePage: NextPage = () => {
           <img className="w-full h-full object-cover" src="https://media.istockphoto.com/id/1290656529/photo/robotic-pneumatic-piston-sucker-unit-on-industrial-machine.jpg?s=612x612&w=0&k=20&c=KfRjZlT6CEX8KpOXylDu_3ggvOftlQF3yh5JVT2KFUw="/>
      
         </div>
-      </div>
-
-      <div className="relative w-full h-screen bg-lime-600">
+        <div className="w-full mx-auto max-w-[84rem]">
+          <div className="absolute flex md:mx-0 mx-auto md:flex-col flex-row gap-5 top-0 z-20">
+            <a href="https://cpacsystems.se/"  target="_blank">
+              <img src="./cpac.png" className="mx-auto h-[12vh]"/>
+            </a>
+            <a href="https://www.tetrapak.com/"  target="_blank">
+              <img src="./TetraPak.png" className="mx-auto h-[20vh]"/>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
     </>
