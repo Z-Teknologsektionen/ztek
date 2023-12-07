4a3a3553fd5d04ef21a456abc32d4d3881ecf72c
Globee <marten.sanderod@gmail.com>
2023-12-07 14:20:41 +0100

    fix linting errors

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -30,31 +30,31 @@ import { cn } from "~/utils/utils";
 
 const quickLinks = [
   {
-    icon: <MdCalendarMonth size={"3em"}/>,
+    icon: <MdCalendarMonth size={"3em"} />,
     href: "https://cloud.timeedit.net/chalmers/web/public/",
     text: "Schema",
     tooltip: "TimeEdit",
   },
   {
-    icon: <MdMeetingRoom size={"3em"}/>,
+    icon: <MdMeetingRoom size={"3em"} />,
     href: "https://cloud.timeedit.net/chalmers/web/b1/",
     text: "Grupprum",
     tooltip: "Boka grupprum på Chalmers",
   },
   {
-    icon: <MdSchool size={"3em"}/>,
+    icon: <MdSchool size={"3em"} />,
     href: "https://www.chalmers.se/utbildning/dina-studier/",
     text: "Studentportalen",
     tooltip: "Här kan du läsa mer om dina studier",
   },
   {
-    icon: <MdAnalytics size={"3em"}/>,
+    icon: <MdAnalytics size={"3em"} />,
     href: "https://stats.ftek.se/",
     text: "Tentastatestik",
     tooltip: "Här kan du se tentastatestik för de flesta kurser på Chalmers.",
   },
   {
-    icon: <MdAccountBalance size={"3em"}/>,
+    icon: <MdAccountBalance size={"3em"} />,
     href: "https://www.student.ladok.se/student/app/studentwebb",
     text: "Ladok",
     tooltip: "Här kan du anmäla dig till tentor och se dina resultat.",
@@ -94,7 +94,6 @@ const StudentPage: NextPage = () => {
                       <IconContext.Provider
                         value={{
                           color: "black",
-
                         }}
                       >
                         {link.icon}
