cf7a3ff5fe5ed9cd033cb084f56cc120ca3fd3fd
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2023-12-26 22:05:10 +0100

    Fix: Remove scrollbar issue

--- src/pages/active/index.tsx
+++ src/pages/active/index.tsx
@@ -34,24 +34,24 @@ const AdminHomePage: NextPage = () => {
           color="white"
         />
       </div>
-      <Tabs className="w-full" defaultValue={initialRoute?.name}>
+      <Tabs defaultValue={initialRoute?.name}>
         {/* TODO: Add scrollbar if to many values */}
-        <TabsList
-          className={`xl:px-4" z-30 flex h-12 w-full space-x-4 overflow-auto rounded-none bg-zBlack px-4 text-white md:px-6`}
-        >
-          {availableRoutes.map((route) => {
-            return (
-              <TabsTrigger
-                key={route.name}
-                className="mx-2 min-w-fit overflow-hidden"
-                style={{ textOverflow: "ellipsis", whiteSpace: "nowrap" }}
-                value={route.name}
-              >
-                {route.name}
-              </TabsTrigger>
-            );
-          })}
-        </TabsList>
+        <div className="w-full overflow-x-scroll ">
+          <TabsList className="z-30 h-12 rounded-none bg-zBlack px-4 text-white md:px-6 xl:px-4">
+            {availableRoutes.map((route) => {
+              return (
+                <TabsTrigger
+                  key={route.name}
+                  className="flex min-w-fit overflow-hidden"
+                  style={{ textOverflow: "ellipsis", whiteSpace: "nowrap" }}
+                  value={route.name}
+                >
+                  {route.name}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+        </div>
         {availableRoutes.map((route) => {
           return (
             <TabsContent key={route.name} value={route.name}>
