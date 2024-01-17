f520171561afb7ab747731c9c57fb3f72009696f
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-01-17 09:21:54 +0100

    Feat: Add redirects to minimize broken links. Moved to next config file

--- next.config.mjs
+++ next.config.mjs
@@ -28,10 +28,34 @@ const config = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'zfoto.ztek.se',
+        protocol: "https",
+        hostname: "zfoto.ztek.se",
       },
     ],
   },
+  async redirects() {
+    // https://nextjs.org/docs/app/api-reference/next-config-js/redirects
+    const oldRoutesRedirects = Object.entries(oldRoutesMap).map(
+      ([oldRoute, newRoute]) => ({
+        source: oldRoute,
+        destination: newRoute,
+        permanent: true,
+      }),
+    );
+
+    return [...oldRoutesRedirects];
+  },
 };
 export default config;
+
+const oldRoutesMap = {
+  // För att förstöra så få gamla länkar som möjligt från gamla ztek
+  // old path: new path
+  "/zaloonen": "/student-division/zaloonen",
+  "/om-z": "/student-division",
+  "/dokument": "/documents",
+  "/members/request": "/active",
+  "/foretag/tjanster": "/business",
+  "/kommitteer": "/student-division/committees",
+  "/kommitteer/:path": "/student-division/committees/:path",
+};
