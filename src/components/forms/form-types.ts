b5643632d913a7f71f3d06ed208c7d5b447df026
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-02-12 17:16:14 +0100

    fix: Remove deeppartial from IUpsertForm

--- src/components/forms/form-types.ts
+++ src/components/forms/form-types.ts
@@ -9,11 +9,8 @@ import type {
 } from "react-hook-form";
 import type { z } from "zod";
 
-type DeepPartial<T> = {
-  [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
-};
 export interface IUpsertForm<schema extends z.ZodObject<z.ZodRawShape>> {
-  defaultValues?: DeepPartial<z.infer<schema>>;
+  defaultValues?: z.infer<schema>;
   formType: "create" | "update";
   onSubmit: (props: z.infer<schema>) => void;
 }
