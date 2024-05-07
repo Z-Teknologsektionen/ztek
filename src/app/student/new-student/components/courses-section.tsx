377a13978086d52a67a6285491ea44e1bbc0e2c1
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-05-07 22:13:11 +0200

    feat: Initial move of new student page to app router

---
+++ src/app/student/new-student/components/courses-section.tsx
@@ -0,0 +1,94 @@
+import type { FC } from "react";
+import SectionTitle from "~/components/layout/section-title";
+import SectionWrapper from "~/components/layout/section-wrapper";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+import { TOOLTIP_DELAY_MS } from "~/constants/delay-constants";
+import { courseData } from "~/data/course-data";
+
+const CoursesSection: FC = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle className="text-center">Kurser</SectionTitle>
+      <p className="text-left">
+        På Chalmers inleds civilingenjörsprogrammen med tre år av varierande
+        studier som bygger upp en gedigen teknisk kompetens. Första året på
+        Automation & Mekatronik fokuserar på grundläggande ämnen som matematik,
+        programmering och fysik. Under det andra året introduceras kärnkurser
+        inom automation, reglerteknik och elektronik. Det tredje året markerar
+        en fördjupning genom avancerade kurser inom mekatronik, realtidsystem
+        och instrumentering. Under detta år får studenten välja kurser som
+        passar deras intressen, och under våren genomförs kandidatarbete. Efter
+        dessa tre år väljer studenterna ett masterprogram för ytterligare
+        specialisering och fördjupning inom sitt valda område. Denna
+        strukturerade progression ger studenterna möjlighet att successivt
+        specialisera sig och skapar en stark teknisk grund för framtida studier
+        och en karriär inom automations- och mekatroniksektorn. Nedan följer en
+        översikt över de kurser som ingår under de tre första åren, tryck på en
+        kurs för att läsa mer
+      </p>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+        {Object.entries(courseData).map(([year, periods], yearIndex) => (
+          <div key={yearIndex} className="rounded border p-4 shadow">
+            <h2 className="mb-2 text-center text-xl font-bold">{year}</h2>
+            {Object.entries(periods).map(([period, courses], periodIndex) => (
+              <div key={periodIndex} className="mb-4">
+                <h3 className="mb-1 border-b border-gray-400 text-lg font-semibold">
+                  {period}
+                </h3>
+                {courses.map(
+                  ({ courseName, points, followUp, url }, courseIndex) => (
+                    <div
+                      key={courseIndex}
+                      className="mb-1 flex flex-row justify-between text-slate-500"
+                    >
+                      <div className="shrink">
+                        <TooltipProvider delayDuration={TOOLTIP_DELAY_MS}>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <a
+                                className="line-clamp-1 text-left font-medium hover:text-blue-500"
+                                href={url}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                {courseName}
+                              </a>
+                            </TooltipTrigger>
+                            <TooltipContent>{courseName}</TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </div>
+                      <div className="flex shrink-0 items-center">
+                        {followUp && (
+                          <em className="mr-2 text-xs text-red-500">
+                            Fortsättning
+                          </em>
+                        )}
+                        <p className="mr-2 text-sm text-gray-600">
+                          {points} HP
+                        </p>
+                      </div>
+                    </div>
+                  ),
+                )}
+                {courses.length < 3 &&
+                  Array.from({ length: 3 - courses.length }).map((_, i) => (
+                    <div key={i} className="invisible">
+                      Invisible Course
+                    </div>
+                  ))}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </SectionWrapper>
+  );
+};
+
+export default CoursesSection;
---
+++ src/app/student/new-student/components/reception-section.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import type { FC } from "react";
+import ImageWithCredit from "~/components/layout/image-with-credit";
+import SectionTitle from "~/components/layout/section-title";
+import SectionWrapper from "~/components/layout/section-wrapper";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/utils/utils";
+
+const ReceptionSection: FC = () => {
+  return (
+    <SectionWrapper>
+      <div className="grid grid-cols-3">
+        <div className="order-first col-span-3 pl-4 lg:order-last lg:col-span-2">
+          <SectionTitle className="mb-4">Mottagningen</SectionTitle>
+          <p className="mr-0 md:mr-12">
+            Mottagningen syftar till att underlätta din integration både socialt
+            och akademiskt. Detta åstadkoms genom fyra intensiva veckor av
+            engagerande aktiviteter. Genom varierande aktiviteter får du
+            möjlighet att lära känna dina medstudenter på sektionen. Det
+            viktigaste är att du närmar dig denna period med en positiv
+            inställning och är redo att njuta av varje stund! Det är normalt att
+            känna nervositet inför mottagningen, men kom ihåg att du inte är
+            ensam. Mottagningskommiten och deras phaddrar kommer göra sitt bästa
+            för att se till att din mottagning blir fantastisk, men det är du
+            som formar din egen upplevelse. Så kom med ett öppet sinne och var
+            redo att ha roligt!
+          </p>
+          <div className="mr-0 mt-8 grid grid-cols-2 gap-8 md:mr-2">
+            <div className="border-gradient col-span-2 rounded-lg md:col-span-1">
+              <h3 className="mb-4 text-xl font-semibold">
+                Samling på Götaplatsen första dagen
+              </h3>
+              <p>
+                Första dagen, 15 augusti 2023, är det samling på Götaplatsen.
+                Efter en festlig välkomstceremoni tågar alla tillsammans upp
+                till Chalmers campus. Därefter börjar mottagningen.
+              </p>
+            </div>
+            <div className="border-gradient col-span-2 rounded-lg md:col-span-1">
+              <h3 className="mb-4 text-xl font-semibold">Redan antagen?</h3>
+              <p>
+                Tryck på denna knapp om du är antagen för mer information från
+                vår mottagningskommitté.
+              </p>
+              <Link
+                className={cn(
+                  buttonVariants({ variant: "outline" }),
+                  "mx-auto mt-4 block",
+                )}
+                href="https://www.znollk.se/"
+                referrerPolicy="no-referrer"
+                target="_blank"
+              >
+                Antagen
+              </Link>
+            </div>
+          </div>
+        </div>
+        <ImageWithCredit
+          alt="Nollbricka som görs på mottagningen"
+          className="order-last col-span-3 m-auto mt-4 lg:order-last lg:col-span-1 lg:mt-0"
+          height={400}
+          photoCommittee="zFoto"
+          photographer="Dennis Holmström"
+          src="/nollbricka.jpg"
+          width={400}
+        />
+      </div>
+    </SectionWrapper>
+  );
+};
+
+export default ReceptionSection;
