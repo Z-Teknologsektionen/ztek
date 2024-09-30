c6e45fa2b3f9ba689ac27a163d37cb0de62e914a
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-09-30 15:20:21 +0200

    feat(Program Board): new caching on program board

--- src/app/student/utils/get-program-board-member-by-role.ts
+++ src/app/student/utils/get-program-board-member-by-role.ts
@@ -1,17 +1,44 @@
+import { unstable_cache } from "next/cache";
 import { prisma } from "~/server/db";
 
+// https://nextjs.org/docs/app/api-reference/functions/unstable_cache
+/* Inte optimal lösning men förhoppningsvis blir denna stabil inom en snar framtid */
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const getProgramBoardMemberByRole = (role: string) =>
-  prisma.programBoardMember.findFirst({
-    where: {
-      role,
-    },
-    select: {
-      name: true,
-      email: true,
-      image: true,
-      phone: true,
-      role: true,
-      url: true,
-    },
-  });
+export const getProgramBoardMemberByRole = unstable_cache(
+  (role: string) =>
+    prisma.programBoardMember.findFirst({
+      where: {
+        role,
+      },
+      select: {
+        name: true,
+        email: true,
+        image: true,
+        phone: true,
+        role: true,
+        url: true,
+      },
+    }),
+  ["getProgramBoardMemberByRole"],
+  { revalidate: false, tags: ["boardProgramMembers"] },
+);
+
+// https://nextjs.org/docs/app/api-reference/functions/unstable_cache
+/* Inte optimal lösning men förhoppningsvis blir denna stabil inom en snar framtid */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const getProgramBoard = unstable_cache(
+  () =>
+    prisma.programBoardMember.findMany({
+      select: {
+        name: true,
+        role: true,
+        phone: true,
+        email: true,
+        url: true,
+        image: true,
+        order: true,
+      },
+    }),
+  ["getProgramBoard"],
+  { revalidate: false, tags: ["boardProgramMembers"] },
+);
