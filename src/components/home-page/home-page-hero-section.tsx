bec0ef689fd9a7b5851ec82e90847dd0b556412d
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-11-21 09:31:30 +0100

    style: remove eslint error

--- src/components/home-page/home-page-hero-section.tsx
+++ src/components/home-page/home-page-hero-section.tsx
@@ -17,7 +17,7 @@ const HomePageHeroSection: FC = () => {
                   Välkommen till Automation och Mekatronik på Chalmers tekniska
                   högskola!
                 </div>
-                <div className="text-balance col-start-1 row-start-5 text-sm md:col-start-1 md:row-start-2 md:text-base xl:text-xl">
+                <div className="col-start-1 row-start-5 text-balance text-sm md:col-start-1 md:row-start-2 md:text-base xl:text-xl">
                   Z-teknologsektionen, eller Z som programmet kallas, är
                   civilingengörsprogrammet på Chalmers som beskrivs som länken
                   mellan maskin-, elektro och datateknik.
