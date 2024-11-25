1465a4938c24bef1199a61385818e90e2493ea1b
Dennis Holmström <85495097+DHolmstrom@users.noreply.github.com>
2024-11-25 21:13:15 +0100

    feat: Better text size, font and updated text

--- src/app/(homepage)/_components/home-page-hero-section.tsx
+++ src/app/(homepage)/_components/home-page-hero-section.tsx
@@ -5,15 +5,17 @@ import { HomePageHeroSponsors } from "./home-page-hero-sponsors";
 
 export const HomePageHeroSection: FC = () => {
   return (
-    <div className="mx-auto mt-4 flex max-w-7xl flex-col gap-4 px-6 py-4 md:grid md:grid-cols-2 md:gap-6 md:px-4 xl:px-2 2xl:px-0">
+    <div className="mx-auto mt-4 flex max-w-7xl flex-col gap-4 px-6 py-4 md:grid md:grid-cols-2 md:gap-8 md:px-4 xl:px-2 2xl:px-0">
       <div className="flex flex-col gap-2">
-        <h1 className="text-balance text-center text-3xl font-semibold">
-          Välkommen till Automation och Mekatronik!
+        <h1 className="text-balance text-center align-text-bottom text-xl font-bold md:text-left md:text-2xl xl:text-4xl">
+          Välkommen till <br className="hidden md:block" />
+          Automation och Mekatronik <br />
+          på Chalmers tekniska högskola!
         </h1>
-        <p className="text-center text-sm font-light lg:text-base">
-          Z-teknologsektionen, eller Z som programmet kallas, är
-          civilingengörsprogrammet på Chalmers tekniska högskola som beskrivs
-          som länken mellan maskin-, elektro och datateknik.
+        <p className="text-balance text-center text-sm md:text-left md:text-base lg:text-base xl:text-xl">
+          Z-teknologsektionen är till för chalmersstudenter som studerar
+          Automation och mekatronik, eller Z som det också kallas. Programmet
+          beskrivs som länken mellan maskin-, elektro- och datateknik.
         </p>
       </div>
       <div className="flex flex-col gap-4 md:row-span-3 md:gap-2">
