a7fa863d5fc14545d79dd532c4263bad9fd8068c
FelixHP05 <felix_hp@outlook.com>
2025-10-23 13:54:58 +0200

    cn docstring

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Very fancy tailwind merge utility function
+ * @param inputs - TailWind values as strings, arrays of strings, dictoinaries of strings and conditions, etc etc...
+ * @returns a TailWind string
+ */
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
