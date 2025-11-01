b2805d110f9cf301774d248aa3928a00fc372ac5
FelixHP05 <felix_hp@outlook.com>
2025-11-01 18:56:57 +0100

    Calendar subscription instructions

---
+++ src/app/student/schedule/components/subscription-instructions-section.tsx
@@ -0,0 +1,94 @@
+import Image from "next/image";
+import { type FC } from "react";
+import SectionTitle from "~/components/layout/section-title";
+import SectionWrapper from "~/components/layout/section-wrapper";
+import StyledLink from "~/components/layout/styled-link";
+
+const SubscriptionInstructionsSection: FC = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle>Prenumerera på kalendrar</SectionTitle>
+      <p>
+        Prenumerationslänkar är länkar som leder till .ics kalenderfiler som
+        emellanåt uppdateras, vanligen för att kalendern ska visa schemat för
+        aktuell tidsperiod.
+      </p>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div className="order-last col-span-3 lg:order-last lg:col-span-2">
+          <p>
+            För att få en prenumerationslänk från TimeEdit, kan följande göras:
+          </p>
+          <ol className="mt-2 list-decimal pl-6">
+            <li>
+              Gå till{" "}
+              <StyledLink
+                href={"https://cloud.timeedit.net/chalmers/web/student/"}
+              >
+                Chalmers TimeEdit
+              </StyledLink>
+            </li>
+            <li>
+              Leta fram ditt schema (om du inte vet vilken klass du år i, går du
+              troligen i TKAUT-1)
+            </li>
+            <li>Tryck på "Prenumerera" i det övre verktygsfältet</li>
+            <li>Kopiera länken i popup-fönstret</li>
+          </ol>
+        </div>
+        <Image src="" alt="Instruktioner för kalenderprenumeration" />
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div className="order-last col-span-3 lg:order-first lg:col-span-2">
+          <p>
+            För att få din Google-kalender att prenumerera på en kalender, kan
+            följande göras i webbgränssnittet:
+          </p>
+          <ol className="mt-2 list-decimal pl-6">
+            <li>
+              Gå till{" "}
+              <StyledLink href={"calendar.google.com"}>
+                Google kalender
+              </StyledLink>
+            </li>
+            <li>Tryck på "+" under "Andra kalendrar" i sidopanelen</li>
+            <li>Välj "Från webbadress" i menyn</li>
+            <li>
+              Klistra in en prenumerationslänk i fältet "Kalenderns webbadress"
+            </li>
+            <li>Tryck "Lägg till kalender"</li>
+          </ol>
+        </div>
+        <Image src="" alt="Instruktioner för kalenderprenumeration" />
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div className="order-last col-span-3 lg:order-last lg:col-span-2">
+          <p>
+            För att få en prenumerationslänk från TimeEdit, kan följande göras:
+          </p>
+          <ol className="mt-2 list-decimal pl-6">
+            <li>
+              Gå till{" "}
+              <StyledLink
+                href={"https://cloud.timeedit.net/chalmers/web/student/"}
+              >
+                Chalmers TimeEdit
+              </StyledLink>
+            </li>
+            <li>
+              Leta fram ditt schema (om du inte vet vilken klass du år i, går du
+              troligen i TKAUT-1)
+            </li>
+            <li>Tryck på "Prenumerera" i det övre verktygsfältet</li>
+            <li>Kopiera länken i popup-fönstret</li>
+          </ol>
+        </div>
+        <Image src="" alt="Instruktioner för kalenderprenumeration" />
+      </div>
+    </SectionWrapper>
+  );
+};
+
+export default SubscriptionInstructionsSection;
--- src/app/student/schedule/page.tsx
+++ src/app/student/schedule/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import type { FC } from "react";
 import ScheduleLinkGeneratorSection from "./components/schedule-link-generator-section";
+import SubscriptionInstructionsSection from "./components/subscription-instructions-section";
+import TimeEditIntroductionSection from "./components/timeedit-introduction-section";
 
 export const metadata: Metadata = {
   title: "Schema",
@@ -8,9 +10,11 @@ export const metadata: Metadata = {
 
 const SchedulePage: FC = () => {
   return (
-    <>
+    <div className="container mx-auto mt-8 divide-y-4 divide-zDarkGray divide-opacity-20">
+      <TimeEditIntroductionSection />
+      <SubscriptionInstructionsSection />
       <ScheduleLinkGeneratorSection />
-    </>
+    </div>
   );
 };
 
