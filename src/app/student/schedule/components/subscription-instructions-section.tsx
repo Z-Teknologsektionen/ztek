bfa192b187e58bce4b03b5c311057eaa113957c5
FelixHP05 <felix_hp@outlook.com>
2025-11-01 21:42:16 +0100

    cal instruction images

--- src/app/student/schedule/components/subscription-instructions-section.tsx
+++ src/app/student/schedule/components/subscription-instructions-section.tsx
@@ -14,8 +14,8 @@ const SubscriptionInstructionsSection: FC = () => {
         aktuell tidsperiod.
       </p>
 
-      <div className="grid grid-cols-3 gap-4">
-        <div className="order-last col-span-3 lg:order-last lg:col-span-2">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+        <div className="order-last col-span-1 lg:order-last lg:col-span-2">
           <p>
             För att få en prenumerationslänk från TimeEdit, kan följande göras:
           </p>
@@ -36,11 +36,16 @@ const SubscriptionInstructionsSection: FC = () => {
             <li>Kopiera länken i popup-fönstret</li>
           </ol>
         </div>
-        <Image src="" alt="Instruktioner för kalenderprenumeration" />
+        <Image
+          src="/TimeEdit_instructions.png"
+          width={689}
+          height={531}
+          alt="Instruktioner för kalenderprenumeration"
+        />
       </div>
 
-      <div className="grid grid-cols-3 gap-4">
-        <div className="order-last col-span-3 lg:order-first lg:col-span-2">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+        <div className="order-last col-span-1 lg:order-first lg:col-span-2">
           <p>
             För att få din Google-kalender att prenumerera på en kalender, kan
             följande göras i webbgränssnittet:
@@ -60,32 +65,12 @@ const SubscriptionInstructionsSection: FC = () => {
             <li>Tryck "Lägg till kalender"</li>
           </ol>
         </div>
-        <Image src="" alt="Instruktioner för kalenderprenumeration" />
-      </div>
-
-      <div className="grid grid-cols-3 gap-4">
-        <div className="order-last col-span-3 lg:order-last lg:col-span-2">
-          <p>
-            För att få en prenumerationslänk från TimeEdit, kan följande göras:
-          </p>
-          <ol className="mt-2 list-decimal pl-6">
-            <li>
-              Gå till{" "}
-              <StyledLink
-                href={"https://cloud.timeedit.net/chalmers/web/student/"}
-              >
-                Chalmers TimeEdit
-              </StyledLink>
-            </li>
-            <li>
-              Leta fram ditt schema (om du inte vet vilken klass du år i, går du
-              troligen i TKAUT-1)
-            </li>
-            <li>Tryck på "Prenumerera" i det övre verktygsfältet</li>
-            <li>Kopiera länken i popup-fönstret</li>
-          </ol>
-        </div>
-        <Image src="" alt="Instruktioner för kalenderprenumeration" />
+        <Image
+          src="/GCal_instructions.png"
+          alt="Instruktioner för kalenderprenumeration"
+          height={853}
+          width={840}
+        />
       </div>
     </SectionWrapper>
   );
