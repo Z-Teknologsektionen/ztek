e6fc0d42aaabf30cd201f2619638370e52041319
FelixHP05 <felix_hp@outlook.com>
2025-11-02 19:46:19 +0100

    bugfix

--- src/server/api/routers/committee.ts
+++ src/server/api/routers/committee.ts
@@ -164,7 +164,10 @@ export const committeeRouter = createTRPCRouter({
   updateCommitteeAsActive: protectedProcedure
     .input(updateCommitteeAsActiveSchema)
     .mutation(
-      async ({ ctx, input: { id, description, image, socialLinks } }) => {
+      async ({
+        ctx,
+        input: { id, description, image, socialLinks, showOldCommittee },
+      }) => {
         const updatedCommittee = await ctx.prisma.committee.update({
           where: {
             id,
@@ -174,6 +177,7 @@ export const committeeRouter = createTRPCRouter({
             image,
             socialLinks,
             updatedByEmail: ctx.session.user.email,
+            showOldCommittee,
           },
         });
 
