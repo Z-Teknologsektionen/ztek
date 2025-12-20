49aa01a11a1d9249c12f8b86c41e1999d12f3c50
FelixHP05 <felix_hp@outlook.com>
2025-12-20 22:25:16 +0100

    webpack bundler flag

--- next.config.mjs
+++ next.config.mjs
@@ -29,6 +29,8 @@ const config = {
       },
     ],
   },
+  // turbopack bundler is currently not in use yet (legacy webpack still in use (probably)). Bundler is chosen from dev/build script in package.json.
+  turbopack: {},
   webpack: (config) => {
     config.module.rules.push({
       test: /\.node$/,
