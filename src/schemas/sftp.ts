48f67aea3b8eac1b1c0322b5ea204d1c9837517b
FelixHP05 <felix_hp@outlook.com>
2026-01-11 14:01:38 +0100

    sftpAPIErrorResponseSchema change

--- src/schemas/sftp.ts
+++ src/schemas/sftp.ts
@@ -12,7 +12,23 @@ import {
 } from "./helpers/sftp-zod-helpers";
 // This file contains Zod schemas for SFTP api
 
+//Helpers
+const sftpAPIErrorCode = z.enum([
+  "FILE_NOT_FOUND",
+  "FILE_NOT_ON_SERVER",
+  "UNEXPECTED_ERROR",
+]);
+class SftpAPIError extends Error {
+  code: z.infer<typeof sftpAPIErrorCode>;
+  constructor(message: string, code: z.infer<typeof sftpAPIErrorCode>) {
+    super(message);
+    this.code = code;
+  }
+}
+
 // Schemas for parsing (client --> server) requests
+// should match call signatures in `src/app/api/sftp/utils/sftp-engine.ts`
+
 export const sftpUploadNewFileSchema = z.object({
   file: sftpFile,
   dir: sftpDir,
@@ -36,11 +52,14 @@ export const sftpDeleteFileSchema = z.object({
 
 // Schemas for parsing (server --> client) responses
 // client expects success if HTTP status is 2xx and error if not
+// aka `success` field is redundant
+
 export const sftpAPISuccessResponseSchema = z.object({
+  success: z.literal(true),
   url: sftpUrl,
-  success: standardBoolean,
 });
 
 export const sftpAPIErrorResponseSchema = z.object({
-  error: standardString,
+  success: z.literal(false),
+  error: z.object({ code: sftpAPIErrorCode, message: standardString }),
 });
