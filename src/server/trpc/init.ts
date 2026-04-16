cc8e1930e480e30d8e8ddb31799464280ea2b682
FelixHP05 <felix_hp@outlook.com>
2026-04-16 13:10:24 +0200

    replaced procedure builders and middleware

--- src/server/trpc/init.ts
+++ src/server/trpc/init.ts
@@ -1,42 +1,13 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-/**
- * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
- * 1. You want to modify request context (see Part 1).
- * 2. You want to create a new middleware or type of procedure (see Part 3).
- *
- * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
- * need to use are documented accordingly near the end.
- */
 
-/**
- * 1. CONTEXT
- *
- * This section defines the "contexts" that are available in the backend API.
- *
- * These allow you to access things when processing a request, like the database, the session, etc.
- */
+import { initTRPC } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type Session } from "next-auth";
-
-import { getServerAuthSession } from "~/server/auth";
-import { prisma } from "~/server/db";
-
-/**
- * 2. INITIALIZATION
- *
- * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
- * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
- * errors on the backend.
- */
-import { AccountRoles } from "@prisma/client";
-import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
-import {
-  userHasAdminAccess,
-  userHasRequiredRole,
-} from "~/utils/user-has-correct-role";
+import { getServerAuthSession } from "~/server/auth";
+import { prisma } from "~/server/db";
 
 type CreateContextOptions = {
   session: Session | null;
@@ -74,7 +45,10 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
   });
 };
 
-const t = initTRPC.context<typeof createTRPCContext>().create({
+export type TRPCContext = Awaited<ReturnType<typeof createInnerTRPCContext>>;
+
+/** tRPC root object, whose properties are the source of all other tRPC related objects */
+export const trpc = initTRPC.context<TRPCContext>().create({
   transformer: superjson,
   errorFormatter({ shape, error }) {
     return {
@@ -87,96 +61,3 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
     };
   },
 });
-
-/**
- * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
- *
- * These are the pieces you use to build your tRPC API. You should import these a lot in the
- * "/src/server/api/routers" directory.
- */
-
-/**
- * This is how you create new routers and sub-routers in your tRPC API.
- *
- * @see https://trpc.io/docs/router
- */
-export const createTRPCRouter = t.router;
-
-/**
- * Public (unauthenticated) procedure
- *
- * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
- * guarantee that a user querying is authorized, but you can still access user session data if they
- * are logged in.
- */
-export const publicProcedure = t.procedure;
-
-/** Reusable middleware that enforces users are logged in before running the procedure. */
-const enforceUserIsAuthed = t.middleware(({ ctx: { session }, next }) => {
-  if (!session || !session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...session, user: session.user },
-    },
-  });
-});
-
-/**
- * Protected (authenticated) procedure
- *
- * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
- *
- * @see https://trpc.io/docs/procedures
- */
-export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
-
-const enforceUserIsAdmin = t.middleware(({ ctx: { session }, next }) => {
-  if (session === null || !userHasAdminAccess(session.user.roles)) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...session, user: session.user },
-    },
-  });
-});
-
-const enforceUserHasRoleOrAdmin = (role: AccountRoles) =>
-  t.middleware(({ ctx: { session }, next }) => {
-    // is signed in
-    if (!session || !session.user) {
-      throw new TRPCError({ code: "FORBIDDEN" });
-    }
-    // has role or is admin
-    if (!userHasRequiredRole(session.user.roles, role)) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next({
-      ctx: {
-        session: { ...session, user: session.user },
-      },
-    });
-  });
-
-export const adminProcedure = t.procedure.use(enforceUserIsAdmin);
-
-export const programBoardProcedure = t.procedure.use(
-  enforceUserHasRoleOrAdmin(AccountRoles.MODIFY_PROGRAM_BOARD),
-);
-
-export const documentProcedure = t.procedure.use(
-  enforceUserHasRoleOrAdmin(AccountRoles.MODIFY_DOCUMENTS),
-);
-
-export const zenithMediaProcedure = t.procedure.use(
-  enforceUserHasRoleOrAdmin(AccountRoles.MODIFY_ZENITH_MEDIA),
-);
-
-export const organizationManagementProcedure = t.procedure.use(
-  enforceUserHasRoleOrAdmin(AccountRoles.ORGANIZATION_MANAGEMENT),
-);
---
+++ src/server/trpc/procedure-builders.ts
@@ -0,0 +1,74 @@
+import type { AccountRoles } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { objectId } from "~/schemas/helpers/common-zod-helpers";
+import {
+  userHasAdminAccess,
+  userHasRequiredRole,
+} from "~/utils/user-has-correct-role";
+import type { TRPCContext } from "./init";
+import { trpc } from "./init";
+
+const enforceSignedIn = trpc.middleware(({ ctx: { session }, next }) => {
+  if (!session || !session.user) {
+    throw new TRPCError({ code: "FORBIDDEN" });
+  }
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+      session: { ...session, user: session.user },
+    },
+  });
+});
+
+/**
+ * @returns middleware for enforcing user has specified role
+ * @param `role` - Lowest account role needed for authorization (ADMIN, and SUPER_ADMIN is higher)
+ */
+export const enforceRoleOrAdmin = (role: AccountRoles) =>
+  trpc.middleware(({ ctx: { session }, next }) => {
+    // is signed in
+    if (!session || !session.user) {
+      throw new TRPCError({ code: "FORBIDDEN" });
+    }
+    // has role or is admin
+    if (!userHasRequiredRole(session.user.roles, role)) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return next({
+      ctx: {
+        // infers the `session` as non-nullable
+        session: { ...session, user: session.user },
+      },
+    });
+  });
+
+/** No access conditions */
+export const publicProcedure = trpc.procedure;
+
+/** Authorized only if signed in */
+export const protectedProcedure = trpc.procedure.use(enforceSignedIn);
+
+/**
+ * Authorized to mutate ONLY your (by `CommitteeId`) db entries. Procedure input MUST contain `id`.
+ * @param getCommitteeId - function who must return the `committeeId` of db entry, given its `id` and the tRPC context.
+ * @returns a `ProcedureBuilder<...>`.
+ */
+export const committeeProcedure = (
+  getCommitteeId: (ctx: TRPCContext, id: string) => Promise<string | null>,
+) => {
+  return protectedProcedure
+    .input(z.object({ id: objectId }))
+    .use(async ({ ctx, next, input }) => {
+      const usr = ctx.session.user;
+
+      // admins skip checks
+      if (!userHasAdminAccess(usr.roles)) {
+        const committeeId = await getCommitteeId(ctx, input.id); // get owner of db entry (using use case supplied function)
+        if (committeeId === null) throw new TRPCError({ code: "NOT_FOUND" });
+        if (committeeId !== usr.committeeId)
+          throw new TRPCError({ code: "UNAUTHORIZED" }); // if owner is not your committee, fail check
+      }
+      return next();
+    });
+};
